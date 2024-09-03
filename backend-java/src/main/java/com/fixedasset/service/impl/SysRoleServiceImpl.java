bc1265597c52418c48eb68425c6a254c89ca65a9
Felix <53119101+felix9611@users.noreply.github.com>
2024-09-03 13:05:19 -0400

    backend updated

--- backend-java/src/main/java/com/fixedasset/service/impl/SysRoleServiceImpl.java
+++ backend-java/src/main/java/com/fixedasset/service/impl/SysRoleServiceImpl.java
@@ -1,13 +1,19 @@
 package com.fixedasset.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.fixedasset.entity.ActionRecord;
 import com.fixedasset.entity.SysRole;
+import com.fixedasset.mapper.ActionRecordMapper;
 import com.fixedasset.mapper.SysRoleMapper;
 import com.fixedasset.service.SysRoleService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -19,6 +25,10 @@ import java.util.List;
  */
 @Service
 public class SysRoleServiceImpl extends ServiceImpl<SysRoleMapper, SysRole> implements SysRoleService {
+    @Resource SysRoleMapper sysRoleMapper;
+    @Resource ActionRecordMapper actionRecordMapper;
+    @Resource SysRole sysRole;
+    @Resource private ActionRecord actionRecord;
     @Override
     public List<SysRole> listRolesByUserId(Long userId) {
 
@@ -26,5 +36,56 @@ public class SysRoleServiceImpl extends ServiceImpl<SysRoleMapper, SysRole> impl
                 .inSql("id", "select role_id from sys_user_role where user_id = " + userId));
 
         return sysRoles;
+    }
+
+    public String voidById(Long id) {
+        sysRole.setId(id);
+        sysRole.setStatu(0);
+        sysRole.setUpdated(LocalDateTime.now());
+        sysRoleMapper.updateById(sysRole);
+
+        actionRecord.setActionName("Delete Role");
+        actionRecord.setActionMethod("DELETE");
+        actionRecord.setActionFrom("System Role Manger");
+        actionRecord.setActionData(sysRole.toString());
+        actionRecord.setActionSuccess("Success");
+        actionRecord.setCreated(LocalDateTime.now());
+        this.createdAction(actionRecord);
+
+        return "Role id was void:" + id.toString();
+    }
+
+    public SysRole createNewRole(SysRole newData) {
+
+
+        sysRoleMapper.insert(newData);
+
+        actionRecord.setActionName("Created Role");
+        actionRecord.setActionMethod("POST");
+        actionRecord.setActionFrom("System Role Manger");
+        actionRecord.setActionData(sysRole.toString());
+        actionRecord.setActionSuccess("Success");
+        actionRecord.setCreated(LocalDateTime.now());
+
+        return newData;
+    }
+
+    public SysRole updateRole(SysRole data) {
+
+
+        sysRoleMapper.insert(data);
+
+        actionRecord.setActionName("Update Role");
+        actionRecord.setActionMethod("POST");
+        actionRecord.setActionFrom("System Role Manger");
+        actionRecord.setActionData(sysRole.toString());
+        actionRecord.setActionSuccess("Success");
+        actionRecord.setCreated(LocalDateTime.now());
+
+        return data;
+    }
+
+    public int createdAction(ActionRecord actionRecord) {
+        return actionRecordMapper.insert(actionRecord);
     }
 }
