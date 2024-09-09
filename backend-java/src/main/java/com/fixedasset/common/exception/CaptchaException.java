9cd8287d04e911bb85afa8f3c956b9d0069e3e4b
Felix <53119101+felix9611@users.noreply.github.com>
2024-09-08 20:41:24 -0400

    Update CaptchaException.java

--- backend-java/src/main/java/com/fixedasset/common/exception/CaptchaException.java
+++ backend-java/src/main/java/com/fixedasset/common/exception/CaptchaException.java
@@ -4,10 +4,6 @@ import org.springframework.security.core.AuthenticationException;
 
 /**
  * Case Captcha Exception
- *
- * @Author WaiterXiaoYY
- * @Date 2022/1/18 19:37
- * @Version 1.0
  */
 public class CaptchaException extends AuthenticationException {
     public CaptchaException(String msg) {
