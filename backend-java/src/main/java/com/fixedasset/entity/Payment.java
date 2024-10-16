899cf08a14d4c8fae9938dcb0422b6eaa142a098
Felix <53119101+felix9611@users.noreply.github.com>
2024-10-16 09:21:27 -0400

    update for open api

--- backend-java/src/main/java/com/fixedasset/entity/Payment.java
+++ backend-java/src/main/java/com/fixedasset/entity/Payment.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
@@ -13,14 +15,23 @@ import java.time.LocalDateTime;
 @TableName("payment")
 @Component
 public class Payment {
+    @Schema(description = "The data id, Only apply for response and update apis")
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
+
+    @Schema(description = "The invoice data id")
     @TableField("invoice_id")
     private int invoiceId;
+
+    @Schema(description = "The payment method")
     @TableField("method")
     private String method;
+
+    @Schema(description = "The payment amount")
     @TableField("amount")
     private Double amount;
+
+    @Schema(description = "The payment time stamp")
     @TableField("payment_time")
     private LocalDateTime paymentTime;
 }
